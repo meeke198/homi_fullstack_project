137a7aa2b481c2cb3533e072cab8f3dd9d4fc237
hien nguyen <thuhiennguyen198@gmail.com>
2021-11-03 14:54:18 -0400

    add profile

---
+++ frontend/components/profile/profile.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { CgProfile } from "react-icons/cg";
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props;
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+  toggleDropdown(e) {
+    let dropDown = document.getElementsByClassName("drop-down-profile");
+    dropDown.addEvenListener("click", function () {
+      let result = classes.toggle("c");
+      if (result) {
+        span.textContent = `'c' added; classList is now "${classes}".`;
+      } else {
+        span.textContent = `'c' removed; classList is now "${classes}".`;
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="user-profile" style={{ position: "relative" }}>
+        <CgProfile />
+        <div
+          className="drop-down-profile"
+          style={{
+            width: 300,
+            height: 400,
+            background: "white",
+            position: "absolute",
+            left: 0,
+            top: "25px",
+          }}
+        >
+          <p>Profile</p>
+          <p>Purchase and reviews</p>
+          <p>Account setting</p>
+          <p>Logout</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
