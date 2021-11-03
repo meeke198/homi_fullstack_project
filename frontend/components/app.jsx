dff0ce3a2f48960fdd43bf54e23e9b93f79d38af
hien nguyen <thuhiennguyen198@gmail.com>
2021-11-03 16:54:10 -0400

    add not found component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,13 +5,20 @@ import SignUpFormContainer from "./session_form/signup_form_container";
 import LogInFormContainer from "./session_form/login_form_container";
 import NavBarContainer from "./nav_bar/nav_bar_container";
 import Home from "./home/home"
+import NotFound from "./not_found/not_found"
 
 const App = () => (
   <div>
-    <Route exact path="/" component={NavBarContainer} />
+    <header>
+      <Route path="/" component={NavBarContainer} />
+    </header>
+    <Switch>
+      {/* <Route path="/" component={NavBarContainer} /> */}
       <AuthRoute exact path="/login" component={LogInFormContainer} />
       <AuthRoute exact path="/signup" component={SignUpFormContainer} />
-      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/" component={Home} /> */}
+      <Route exact={true} path="*" component={NotFound} />
+    </Switch>
   </div>
 );
 
--- frontend/components/not_found/not_found.jsx
+++ frontend/components/not_found/not_found.jsx
@@ -1,3 +1,13 @@
 import React from 'react';
 
 
+const NotFound = () => {
+    return (
+        <div>
+        <h3>404 page not found</h3>
+        <p>This page does not exit</p>
+        </div>
+
+    )
+}
+export default NotFound;
