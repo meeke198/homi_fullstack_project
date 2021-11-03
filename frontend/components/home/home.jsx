d6064886a75b97f458322ca139321d96e35a6b05
hien nguyen <thuhiennguyen198@gmail.com>
2021-11-03 15:14:35 -0400

    add home.jsx

---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="product-page">This is Home Page for Product</div>;
+  }
+}
+
+export default Home;
