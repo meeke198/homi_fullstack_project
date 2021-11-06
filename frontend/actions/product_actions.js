b33ca9924addc8fa15b18379c62c3ef67456f974
hien nguyen <thuhiennguyen198@gmail.com>
2021-11-06 19:34:17 -0400

    add product util and actions

---
+++ frontend/actions/product_actions.js
@@ -0,0 +1,24 @@
+import * as ProductApiUtil from "../util/product_util"
+
+export const RECEIVE_ALL_PRODUCTS = "RECEIVE_ALL_PRODUCTS";
+export const RECEIVE_PRODUCT = "RECEIVE_PRODUCT";
+
+export const receiveAllProducts = products => ({
+    type: RECEIVE_ALL_PRODUCTS,
+    products
+})
+
+export const receiveProduct = product => ({
+    type: RECEIVE_ALL_PRODUCTS,
+    product
+})
+
+export const fetchProducts = () => dispatch => {
+    return ProductApiUtil.fetchProducts()
+    .then(products => dispatch(receiveAllProducts(products)));
+};
+
+export const fetchProduct = productId => dispatch => {
+    return ProductApiUtil.fetchProduct(productId)
+    .then(productId => dispatch(receiveProduct(productId)))
+}
