868bdb4cc8601188ee64115afd35f485f923acc2
hien nguyen <thuhiennguyen198@gmail.com>
2021-12-22 16:31:30 -0500

    add item counter

--- frontend/components/carts/item_counter.jsx
+++ frontend/components/carts/item_counter.jsx
@@ -1,16 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Route } from "react-router";
+import { BsCart4 } from "react-icons/bs";
 
 const ItemCounter = ({ items, currentUser, openModal }) => {
   const allCartItems = Object.values(items)
   let totalItems = 0;
   allCartItems.forEach((item) => (totalItems += item.quantity));
 
-  return (
+const loggedIn = () => (
+  <div>
+    <Link to="/cart_items">
+      <BsCart4
+        style={{
+          marginLeft: 15,
+          width: 30,
+          height: 30,
+          textDecoration: "none",
+          position: "relative",
+        }}
+      />
+    </Link>
+    <div className="counter">{isNaN(totalItems) ? 0 : totalItems}</div>
+  </div>
+);
+const loggedOut = () => (
+  <div>
     <div>
-      <div>{isNaN(totalItems) ? 0 : totalItems} items in your cart</div>
-      <div>keep shopping</div>
+      <BsCart4
+      onClick = {() => openModal("login")}
+        style={{
+          marginLeft: 15,
+          width: 30,
+          height: 30,
+          textDecoration: "none",
+          position: "relative",
+        }}
+      />
     </div>
-  )}
+    <div className="counter">{isNaN(totalItems) ? 0 : totalItems}</div>
+  </div>
+)
+
+  return currentUser ? loggedIn() : loggedOut();
+}
 export default ItemCounter;
