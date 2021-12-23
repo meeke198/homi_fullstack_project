cc43e015f6fb1fb6030988b721a079c6ae545079
hien nguyen <thuhiennguyen198@gmail.com>
2021-12-22 22:50:48 -0500

    create cart show item

--- frontend/components/carts/cart_show_item.jsx
+++ frontend/components/carts/cart_show_item.jsx
@@ -9,49 +9,54 @@ class CartShowItem extends React.Component {
     };
   }
   componentDidMount() {
-    this.props.fetchCartItems(this.props.cartItems);
     if (this.props.currentUser) {
-      this.props.fetchCart(this.props.currentUser.id);
+      this.props.fetchCartItems();
     }
   }
 
-  addItemToCartHandler(e) {
-    e.preventDefault();
-    if (this.props.currentUser) {
-      const cartItem = {
-        cart_id: this.props.currentUser.id,
-        product_id: this.props.product.id,
-        quantity: this.state.quantity,
-      };
-      this.props.thunkCreateCartItem(cartItem);
-    } else {
-      this.setState({ errors: "Please log in first!" });
-      this.props.openModal("Login");
-    }
-    // this.props.thunkCreateCartItem(this.props.product.id, this.state.quantity);
-  }
-
+  
   quantityHandler(e) {
     this.setState({ quantity: e.target.value });
   }
-  // addToCartSubmit(e) {
-  //   e.preventDefault();
-  //   if (this.props.currentUser) {
-  //     this.props.createCartItem(this.props.product?.id, this.state.quantity);
-  //   }
-  // }
+  
   render() {
     const { cartItems } = this.props;
     let allItems =
       Object.values(cartItems) === 0 ? [] : Object.values(cartItems);
     let totalItemsInCart = 0;
     allItems.forEach((item) => totalItemsInCart += item.quantity)
-    let allProductsArray = [;]
-    return (
-      <div className="cart-show-container">
-       <h1>{}</h1>
-      </div>
-    );
+    let allProductsArray = [];
+    allItems.forEach((item) => {
+      if (allProductsArray.includes(item.product.product_name)){
+        item.product.product_name
+      }
+    })
+
+    let itemsPrice = 0;
+    allItems.forEach((item) => itemsPrice += (item.product.price * item.quantity));
+    if (cartItems === undefined){
+      return null
+    }
+
+    let tax = itemsPrice * 0.08
+    let total = itemsPrice + tax
+    if (totalItemsInCart === 0) {
+      return (
+        <div>
+          <h1>Your cart is empty</h1>
+          <Link>Discover something unique to fill it up</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div className="cart-show-container">
+          <h1>{totalItemsInCart} items in your cart</h1>
+          <p>Keep shopping</p>
+        </div>
+      );
+    }
+
+    
   }
 }
 
