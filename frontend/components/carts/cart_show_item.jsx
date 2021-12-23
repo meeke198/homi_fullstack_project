1fc92dd906b642142e6252c17c6e0f7012fc62e7
hien nguyen <thuhiennguyen198@gmail.com>
2021-12-23 13:43:25 -0500

    add updateQuantity

--- frontend/components/carts/cart_show_item.jsx
+++ frontend/components/carts/cart_show_item.jsx
@@ -10,11 +10,13 @@ class CartShowItem extends React.Component {
       product_id: this.props.item.product.product_id,
       quantity: this.props.item.product.quantity,
     };
+    this.updateQuantity = this.updateQuantity.bind(this)
   }
 
 
-  quantityHandler(e) {
-    this.setState({ quantity: e.target.value });
+  updateQuantity(e) {
+    e.preventDefault();
+    this.setState({ quantity: parseInt(e.target.value) });
   }
   
   render() {
@@ -27,7 +29,7 @@ class CartShowItem extends React.Component {
         <div>
           <Link to={`/products/${item.product.product_id}`}>
             <img
-              src={item.image_url}
+              src={item.cart.image_url}
               alt="product_image"
               style={{ width: "200px", height: "200px" }}
             />
@@ -36,10 +38,9 @@ class CartShowItem extends React.Component {
             <p>{item.product.product_name}</p>
           </Link>
           <div>
-            <label htmlFor="">Change quality</label>
-            <select className="item-quantity" onChange={this.quantityHandler}>
+            <select className="item-quantity" onClick={this.updateQuantity}>
               <option value="1" selected>
-                1
+                Qty: 1
               </option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -53,37 +54,3 @@ class CartShowItem extends React.Component {
 }
 
 export default CartShowItem;
-// return (
-//   <div className="product-container-show">
-//     <img className="product-img-show" src={product?.image_url} alt="" />
-//     <div className="product-content-show">
-//       <p className="product-name-show">{product?.product_name}</p>
-//       <br />
-//       <p style={{ fontWeight: 700, paddingBottom: 20 }}>Description:</p>
-//       <p className="product-description">{product?.description}</p>
-//       <p className="price">Price: $ {product?.price}.00</p>
-//       <div className="add_to_cart">
-//         <label style={{ padding: "10px 0" }}>Quantity</label>
-//         <br />
-//         <form
-//           className="custom-select"
-//           style={{ width: "350px" }}
-//           onSubmit={this.addItemToCartHandler}
-//         >
-//           <select className="quantity-select" onChange={this.quantityHandler}>
-//             <option value="1" selected>
-//               1
-//             </option>
-//             <option value="2">2</option>
-//             <option value="3">3</option>
-//             <option value="4">4</option>
-//             <option value="5">5</option>
-//           </select>
-//           <button type="submit" className="submit-form-button">
-//             Add to cart
-//           </button>
-//         </form>
-//       </div>
-//     </div>
-//   </div>
-// );
