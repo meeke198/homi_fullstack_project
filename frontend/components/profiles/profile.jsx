db71766c523ae49f0b439a169c29596618f1eb58
hien nguyen <thuhiennguyen198@gmail.com>
2022-01-07 18:54:42 -0500

    add user profile icons

--- frontend/components/profiles/profile.jsx
+++ frontend/components/profiles/profile.jsx
@@ -1,84 +1,73 @@
 import React from "react";
-import StarRatings from "react-star-ratings";
-class CreateReviewForm extends React.Component {
-  constructor(props) {
-    // debugger
-    super(props);
-    this.state = this.props.review;
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
-    this.changeRating = this.changeRating.bind(this);
-    this.updateContent = this.updateContent.bind(this);
-  }
-  componentDidMount() {
-    // debugger
-    if (this.props.formType === "edit") {
-      debugger;
-      this.props.fetchReview(this.props.match.params.reviewId);
-    }
-  }
-  handleSubmit(e) {
-    // debugger
-    e.preventDefault();
-    this.props.createReview(this.state);
-    this.setState({ rating: 0, content: "" });
-  }
-  changeRating(newRating) {
-    this.setState({ rating: newRating });
-  }
-  updateContent(e) {
-    e.preventDefault();
-    this.setState({ content: e.target.value });
-  }
-  handleUpdate(e) {
-    // debugger
-    e.preventDefault();
-    this.props
-      .updateReview(this.state)
-      .then(() => this.props.history.push("/"));
-  }
+import { MdOutlineEdit } from "react-icons/md";
+import { BsShop, BsCameraFill } from "react-icons/bs";
+import { GrFavorite } from "react-icons/gr";
 
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     // debugger
     if (this.state === null) {
       return null;
     }
-    // const { review } = this.props
-    const submitButton =
-      this.props.formType === "create" ? "Submit Review" : "Edit Review";
-    const submit =
-      this.props.formType === "create" ? this.handleSubmit : this.handleUpdate;
-
+    const { currentUser } = this.props
     return (
-      <form className="review-form" onSubmit={submit}>
-        <h1 className="rating-text">Write a review</h1>
-        <StarRatings
-          style={{ marginLeft: "5px" }}
-          rating={this.state.rating}
-          starRatedColor="#222222"
-          starHoverColor="#222222"
-          changeRating={this.changeRating}
-          numberOfStars={5}
-          name="rating"
-          starDimension="28px"
-          starSpacing="5px"
-        />
-        <div>
-          <textarea
-            cols="50"
-            rows="5"
-            value={this.state.content}
-            onChange={this.updateContent}
-          ></textarea>
+      <div className="profile">
+       
+        <div className="shopping-cart">
+          <img className="image" src="https://homi-seeds.s3.us-east-2.amazonaws.com/vector61-1574-01.jpg" alt="profile" />
+          <BsCameraFill className="camera"/>
         </div>
-        <div className="review-button">
-          <button className="review-submit btn" type="submit">
-            {submitButton}
-          </button>
+        <div className="user-name">
+            <h1>{currentUser.email}</h1>
+            <div><MdOutlineEdit/></div>
         </div>
-      </form>
+        <div className="user-follower">
+            <p>4 followers</p>
+            <p>5 following</p>
+        </div>
+        <div className="profile-icon">
+            <div className="icon-container">
+                <GrFavorite/>
+                 <p>Favorite Items</p>
+                <p>0 Items</p>
+            </div>
+            <div className="icon-container">
+                <BsShop/>
+                <p>Favorite Shop</p>
+                <p>0 Shops</p>
+            </div>
+            <div className="icon-container cross">
+                <h1>+</h1>
+                <p>Create Collection</p>
+            </div>
+        </div>
+        <div className="shopping-cart">
+          <img src="https://homi-seeds.s3.us-east-2.amazonaws.com/shopping-cart-icon.png" alt="shopping-cart" />
+          <h2>Nothing here... yet.</h2>
+          <p>These are a few of your favorite things... or they will be, once you favorite something.
+
+
+
+
+
+Footer
+</p>
+        </div>
+      </div>
     );
   }
 }
 
-export default CreateReviewForm;
+export default Profile;
+
+
+const mSTP = (state) => {
+  return {
+    currentUser: state.entities.users[state.session.id],
+  };
+};
+
+export default connect(mSTP, null)(Profile);
